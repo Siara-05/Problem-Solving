c42a96f008f336945093b160dbba8bc55dc19ae1
Siara-05 <siara240824@gmail.com>
2025-08-08 17:04:22 +0530

    Easy problem

---
+++ Day1_Variables_DataTypes/swap-numbers.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");    Checking if it runs
+
+// PROBLEM STATEMENT :- Define 2 numbers and swap them
+
+
+let a = 5;
+let b = 15;
+
+console.log("Before swaping a =", a , " and  b =", b);
+
+[a,b] = [b,a] ;
+
+console.log ("After swaping a =", a, " and  b =", b);
