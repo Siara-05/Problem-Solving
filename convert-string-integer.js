88dba2231cfe078e5f8832805c7a4c97ddb2e2ee
Siara-05 <siara240824@gmail.com>
2025-08-09 22:30:51 +0530

    converting s - int

---
+++ convert-string-integer.js
@@ -0,0 +1,21 @@
+
+
+// Using Math floor() Method
+
+function convertStoI() {
+    let a = "100";
+    let b = Math.floor(a);
+    console.log(typeof (b));
+}
+convertStoI();
+
+//  Using the Number() function
+
+function convertStoI() {
+    const a = "100";
+    const b = Number(a);
+    console.log(typeof (b));
+    const d = "3 11 43" * 1;
+    console.log(typeof (d));
+}
+convertStoI();
