c1abefd20d39f1866e3f6e50d1a3c568e67bda18
Siara-05 <siara240824@gmail.com>
2025-08-10 15:08:33 +0530

    done

--- Day2_if, else, switch conditions/postive-negative-zero.js
+++ Day2_if, else, switch conditions/postive-negative-zero.js
@@ -1,25 +1,25 @@
 
 // Given a number, check whether it is positive, negative, or zero.
 
-// function PositiveOrNegativeOrZero(a) {
+function PositiveOrNegativeOrZero(a) {
 
-//     if (a > 0) {
-//         return "postivee number";
-//     }
+    if (a > 0) {
+        return "postivee number";
+    }
 
-//     else if (a < 0) {
-//         return "negative number";
-//     }
+    else if (a < 0) {
+        return "negative number";
+    }
 
-//     else {
-//         return "the number is zero";
-//     }
+    else {
+        return "the number is zero";
+    }
 
-// }
+}
 
-// console.log(PositiveOrNegativeOrZero(1));
-// console.log(PositiveOrNegativeOrZero(-1));
-// console.log(PositiveOrNegativeOrZero(0));
+console.log(PositiveOrNegativeOrZero(1));
+console.log(PositiveOrNegativeOrZero(-1));
+console.log(PositiveOrNegativeOrZero(0));
 
 
 //  If we have to take number from user 
