21bfc7d4cb8690403f785b32c30e1509bab5dbfa
Siara-05 <siara240824@gmail.com>
2025-08-12 19:51:11 +0530

    day3

---
+++ Day3_Loops/numbers_1to20.js
@@ -0,0 +1,24 @@
+
+// Write a program that prints numbers from 1 to 20.
+
+// using for loop
+
+for ( let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Using a while loop
+
+let j = 1;
+while (j <= 20) {
+    console.log(j);
+    j++;
+}
+
+//  Using a do...while loop  Ensures the loop runs at least once before checking the condition.
+
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i <= 15);
