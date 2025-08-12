dccbfc05ca85ae18decdf60f4656b5fb833d7890
Siara-05 <siara240824@gmail.com>
2025-08-12 20:25:24 +0530

    day 3 problem 2

---
+++ Day3_Loops/multiplication-table.js
@@ -0,0 +1,9 @@
+
+// Take a number (e.g., 5) and print its multiplication table up to 10.Format the output like 5 x 3 = 15 to make it look real.
+
+let number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log (`${number} * ${i} = ${number *i}`);
+}
+
+// NOTE:- the syntax is (``) not ('')
