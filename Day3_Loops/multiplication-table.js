2442668addd68cdf2ecef8149f5a546575318b7e
Siara-05 <siara240824@gmail.com>
2025-08-13 21:56:28 +0530

    alternative

--- Day3_Loops/multiplication-table.js
+++ Day3_Loops/multiplication-table.js
@@ -7,3 +7,10 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // NOTE:- the syntax is (``) not ('') 
+
+// Alternative:-  String concatenation using +
+
+let number = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(number + " * " + i + " = " + (number * i));
+}
