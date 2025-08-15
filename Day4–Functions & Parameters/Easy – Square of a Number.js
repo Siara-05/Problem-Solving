b8a74a76b2575a05a18094b5d711a33c795d1d52
Siara-05 <siara240824@gmail.com>
2025-08-15 13:54:10 +0530

    day 4 problems start

---
+++ Day4–Functions & Parameters/Easy – Square of a Number.js
@@ -0,0 +1,18 @@
+
+// Write a function square(num) that takes a number and returns its square.
+
+// for ( let i = 1; i <= 2; i++) {
+//          console.log ( "*" );
+    
+//     for ( let j = 1; j <= i; j++) {
+
+//         console.log ( "df" );
+        
+// } }
+
+
+let star = "*";
+
+for ( i=1 ; i <=5; i ++) {
+    console.log (star );
+}
